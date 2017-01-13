7494e2ce4d9ccfdcab3ecfe01a043dcbdd0c0b7d
chrisanthropic <ctarwater@gmail.com>
2017-01-12 19:04:43 -0800

    add plexPy. Fixes #2

--- 02-plex.sh
+++ 02-plex.sh
@@ -40,3 +40,59 @@ cat << EOF
 #        Remote Access-->Enable
 #    Open IPADDRESS:32400/web
 EOF
+
+echo ''
+echo "You need to manually enable remote management for Plex."
+echo "Issue the following command on your LOCAL machine:"
+echo "ssh $username@$ipaddr -L 8888:localhost:32400"
+echo "open `localhost:8888/web` in a browser"
+echo "Either login or create a new Plex account."
+echo "Now enable remote access: Remote Access ->> Enable"
+echo ''
+echo "You should now be able to access plex at: $ipaddr:32400/web"
+echo "Have you enabled remote access?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) ufw allow 8181; echo ''; echo "Cool, moving along."; echo ''; break;;
+        No ) echo "You should really do that before continuing."; exit;;
+    esac
+done
+
+# Install PlexPy
+git clone https://github.com/JonnyWong16/plexpy.git /opt/plexpy/
+chown -R $username:$username /opt/plexpy
+
+# Add Systemd Service File
+tee "/etc/systemd/system/plexpy.service" > /dev/null <<EOF
+[Unit]
+Description=PlexPy - Stats for Plex Media Server usage
+After=plexmediaserver.service
+
+[Service]
+ExecStart=/opt/plexpy/PlexPy.py --quiet --daemon --nolaunch --config /opt/plexpy/config.ini --datadir /opt/plexpy
+GuessMainPID=no
+Type=forking
+User=$username
+Group=$username
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Start Plexpy and enable on boot
+systemctl daemon-reload
+systemctl start plexpy
+systemctl enable plexpy
+
+echo ''
+echo "Do you want to allow remote access to PlexPy?"
+echo "If so, you need to tell UFW to open the port."
+echo "Otherwise, you can use SSH port forwarding."
+echo ''
+echo "Would you like us to open the port in UFW?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) ufw allow 8181; echo ''; echo "Port 8181 open, PlexPy is now available over the internet."; echo ''; break;;
+        No ) echo "Port 8181 left closed. You can still access it on your local machine by issuing the following command: ssh $username@$ipaddr -L 8181:localhost:8181"; echo "and then open localhost:8181 on your browser."; exit;;
+    esac
+done
